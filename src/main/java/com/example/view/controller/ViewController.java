1a0a41aa397094268cbaea2e2dfd096b90791ff6
min-seon-gyu <gcael@naver.com>
2022-10-09 17:44:43 +0900

    커밋

---
+++ src/main/java/com/example/view/controller/ViewController.java
@@ -0,0 +1,40 @@
+package com.example.view.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ViewController {
+
+    @GetMapping("AAA")
+    @ResponseBody
+    public Person helloString() {
+        Person p = new Person();
+        p.setAge(15);
+        p.setName("사람");
+        return p;
+    }
+}
+
+class Person{
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
